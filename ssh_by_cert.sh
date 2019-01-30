3dddd0a5f9c27fca2c009b1929ba5ff3f85c1614
nix89 <37620646+nix89@users.noreply.github.com>
2019-01-30 17:22:37 +0500

    Update ssh_by_cert.sh

--- ssh_by_cert.sh
+++ ssh_by_cert.sh
@@ -1,58 +1,38 @@
 
 #!/bin/bash
-#Включение отладки bash:
 set -x
 
-
-#Учетные данные:
 USER="admin"
-#PASSWD="your pass"
 
-#Файл логов:
 LOG="ssh_conn_by_cert.log"
 
-#Список хостов:
 HOSTS="
 10.10.10.1
 "
 
-#Цикл переборки хостов:
 for H in $HOSTS
 do
 
-#Вывод даты старта скрипта:
 echo START SCRIPT: >> $LOG
 date +%x-%R >> $LOG
 
-#Команды для expect:
+
 COMM="
 
-#Включение и вывод отладки expect:
-#log_file debug.log
-#exp_internal 1
-
-#Время ожидание expect
 set timeout 1
 
-#Соедиение ssh:
 spawn ssh -T $USER@$H
 expect \"*(yes/no)?*\" {send \"yes\r\"}
-#expect \"Password:\"
-#send \"$PASSWD\r\"
 
-#Выполняемые команды:
 expect \"*>\"
 send -- \"/system routerboard print\r\"
 expect \"*>\"
 send \"exit\r\"
 
-#Завершение выполнения expect:
 expect eof
 "
 
-#Запуск expect с набором команд:
 expect -c "$COMM" >> $LOG
-#Вывод разделителя:
 echo ========================================================================= >> $LOG
 
 done
